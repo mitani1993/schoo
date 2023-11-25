a54a5466ecb32d10ec36310ba883f602a203af7a
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-11-25 17:30:17 +0900

    aop joinpoint

--- demo2/src/main/java/com/example/demo2/aop/MessageUtil.java
+++ demo2/src/main/java/com/example/demo2/aop/MessageUtil.java
@@ -1,14 +1,46 @@
 package com.example.demo2.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+/**
+ * メッセージ出力クラス.<br>
+ * 今回はログ出力と見立ててprintlnメソッドを使用します.
+ */
 @Aspect
 @Component
 public class MessageUtil {
-    @Before("execution(* com.example.demo2.service.*.*(..))")
-    public void beforeMessage() {
-        System.out.println("----- Serviceクラス createUser メソッド開始 -----");
+    /**
+     * com.example.demo2パッケージ内のメソッドを開始する際にメッセージを出力します.
+     *
+     * @param joinPoint
+     */
+    @Before("execution(* com.example.demo2.*.*.*(..))")
+    public void beforeMessage(JoinPoint joinPoint) {
+        System.out.println("----- " + joinPoint.getSignature() + "の処理を開始します. −−−−−");
+    }
+
+    /**
+     * com.example.demo2パッケージ内のメソッドを終了する際にメッセージを出力します.
+     *
+     * @param joinPoint
+     */
+    @After("execution(* com.example.demo2.*.*.*(..))")
+    public void afterMessage(JoinPoint joinPoint) {
+        System.out.println("-----" + joinPoint.getSignature() + "の処理を終了します. -----");
+    }
+
+    /**
+     * com.example.demo2.serviceパッケージ内のメソッドがreturnをする際にメッセージを出力します.
+     *
+     * @param joinPoint
+     */
+    @AfterReturning("execution(* com.example.demo2.service.*.*(..))")
+    public void afterMessageReturn() {
+        System.out.println("----- ユーザ情報の処理を終了します. -----");
     }
 }
